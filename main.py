fd026643a6513457c46e0a4505686111ed4c6301
rworsham <robertworsham01@gmail.com>
2024-05-14 23:25:13 -0400

    Further work on overview graph page, stills needs tweaking

--- main.py
+++ main.py
@@ -646,21 +646,18 @@ def settings():
 
 
 def bar_chart_todo():
-    num_work_state = db.session.query(WorkState).all()
-    index = numpy.linspace(1,len(num_work_state),len(num_work_state))
-    print(index)
-    todos = TodoPost.query.order_by(TodoPost.work_state).all()
-    todos_list = [todos]
-    print(todos_list)
-    available_work_states = db.session.query(WorkState).where(WorkState.is_hidden == False).all()
-    columns = [(w.work_state, w.work_state) for w in available_work_states and todos_list]
-    print(columns)
-    df = pd.DataFrame(columns,
-                      columns=['Work-State', "# of To-do's"],
-                      index=index)
-    print(df)
-
-    fig = px.bar(df, x="Work-State", y="'# of To-do's'", color="City", barmode="group")
+    df_todo = pd.read_sql(sql=db.session.query(TodoPost).with_entities(TodoPost.work_state).statement, con=db.engine)
+    df_todo_count = df_todo.groupby("work_state").size()
+    df_todo_graph = df_todo_count.rename("# of To-do's")
+    print(df_todo_graph)
+    df_work_state = pd.read_sql(sql=db.session.query(WorkState).with_entities(WorkState.work_state).order_by(WorkState.work_state_order).statement, con=db.engine)
+    # print(df_work_state)
+    # df_work_state_graph = df_work_state.rename("Work State")
+    df_graph = pd.concat([df_todo_graph,df_work_state ])
+    df_graph = df_graph.reset_index(drop=True)
+    print(df_graph)
+    print(type(df_graph))
+    fig = px.bar(df_graph, x="work_state", y="# of To-do's", labels={'work_state': 'Work State'}, barmode="group")
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
